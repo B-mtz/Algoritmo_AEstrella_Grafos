760cc4c004d3eb522b36f0c68b80e11f7717eb1b
Bernardo <mtzsts1@gmail.com>
2023-09-20 14:35:07 -0600

    Proyecto terminado

--- src/test/Execute.java
+++ src/test/Execute.java
@@ -5,7 +5,8 @@ import logic.GetCitiesLogic;
 import ui.GetCities;
 public class Execute {
     public static void main(String[] args) {
-
+        //Nota: se pueden cargar archivos txt para agregar las cuidades mas rapido
+        //La estructura de cada linea del archivo txt estrictamente tiene que ser la siguiente: Cuidad_Latitud_Longitud
         FlatLightLaf.setup();
         GetCitiesLogic getCitiesLogic = new GetCitiesLogic(new GetCities());
     }
